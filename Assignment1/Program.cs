d3f6593601c20c150ba27b1ac89af4cd91c2600a
rmit-s3177105-yiping-guo <s3177105@student.rmit.edu.au>
2019-01-10 06:36:02 +1100

    Create the program

---
+++ Assignment1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+
+namespace Assignment1
+{
+    public abstract class Person
+    {
+        private String UserID, Name, Email;
+    }
+    public class Staff : Person
+    {
+
+    }
+    public class Student : Person
+    {
+
+    }
+    public class Slot
+    {
+        private String RoomID;
+        private DateTime dateTime;
+    }
+    public class Program
+    {
+        void Initialize()
+        {
+            List<Staff> staffs = new List<Staff>();
+        }
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
